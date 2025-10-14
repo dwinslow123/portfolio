6130036eaf2b7e57c1f6de0cc9c0fb3b9b55bbd3
Dan Winslow <dwinslow@nexient.com>
2025-10-14 14:34:57 -0500

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Nav />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
