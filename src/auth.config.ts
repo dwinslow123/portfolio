869f2e67f91fe530bf69af47544f21d86c3a369b
Dan Winslow <dwinslow@nexient.com>
2025-10-18 12:53:06 -0500

    real quick fix to prevent users from being forced to login

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -7,7 +7,7 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
     const isLoggedIn = !!auth?.user;
-    const isOnDashboard = nextUrl.pathname.startsWith('/');
+    const isOnDashboard = nextUrl.pathname.startsWith('/edit');
 
     if (isOnDashboard) {
       if (isLoggedIn) return true;
